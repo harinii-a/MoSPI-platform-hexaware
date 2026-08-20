741fbfeb7f286e5f93f0eb3534161538e6c96dac
harinii-a <harini.vaidy16@gmail.com>
2026-08-20 15:15:47 +0530

    Update API URL for production

--- frontend/app/src/api.js
+++ frontend/app/src/api.js
@@ -4,7 +4,7 @@
  */
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'https://mospi-platform-hexaware.onrender.com';
 
 const api = axios.create({
   baseURL: BASE_URL,
